c34e339f5af1efde98f2790b0ab91563e31fbc65
Robert Keresnyei <rk@nemron.com>
2026-03-18 20:22:12 +0100

    Add DisallowDeliverNow (#6)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require "rubocop"
 require "rubocop/rspec/support"
 
-require_relative "../rubocop/cops/custom/disallow_postgres_time_functions"
-
 RSpec.configure do |config|
   config.include RuboCop::RSpec::ExpectOffense
 end
