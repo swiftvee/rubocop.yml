f6512c1f217dc7728cdb7ae0a46278c410479c08
Robert Keresnyei <rk@nemron.com>
2026-04-09 10:49:53 +0200

    NoLocalAssigns

---
+++ rubocop/cops/custom/no_local_assigns.rb
@@ -0,0 +1,18 @@
+module RuboCop
+  module Cops
+    module Custom
+      # Disallow local_assigns usage in views/partials.
+      class NoLocalAssigns < ::RuboCop::Cop::Base
+        MSG = "Do not use local_assigns.".freeze
+
+        def on_send(node)
+          return unless node.method?(:local_assigns)
+
+          add_offense(node.loc.selector, message: MSG)
+        end
+
+        alias on_csend on_send
+      end
+    end
+  end
+end
