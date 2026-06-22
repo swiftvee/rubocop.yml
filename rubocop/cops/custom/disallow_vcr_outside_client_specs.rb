a259e11260a2eb46d1acba2193c32a869f1dadb2
Robert Keresnyei <rk@nemron.com>
2026-06-22 12:16:03 +0200

    Disallow using VCR cassettes outside of spec/clients (#16)

---
+++ rubocop/cops/custom/disallow_vcr_outside_client_specs.rb
@@ -0,0 +1,55 @@
+module RuboCop
+  module Cops
+    module Custom
+      # Disallow using VCR cassettes outside of spec/clients.
+      class DisallowVcrOutsideClientSpecs < ::RuboCop::Cop::Base
+        MSG = "Only use VCR cassettes in spec/clients.".freeze
+
+        ALLOWED_PATH = %r{(\A|/)spec/clients/}
+
+        CASSETTE_METHODS = %i[use_cassette insert_cassette].freeze
+
+        EXAMPLE_METHODS = %i[
+          describe context example_group
+          it specify example
+          feature scenario
+          shared_examples shared_context shared_examples_for
+        ].freeze
+
+        def on_send(node)
+          return if allowed_file?
+
+          flag_cassette_call(node)
+          flag_vcr_metadata(node)
+        end
+
+        private
+
+        def flag_cassette_call(node)
+          return unless node.receiver&.const_type? && node.receiver.const_name == "VCR"
+          return unless CASSETTE_METHODS.include?(node.method_name)
+
+          add_offense(node)
+        end
+
+        def flag_vcr_metadata(node)
+          return unless EXAMPLE_METHODS.include?(node.method_name)
+
+          node.arguments.each do |argument|
+            if argument.sym_type? && argument.value == :vcr
+              add_offense(argument)
+            elsif argument.hash_type?
+              argument.pairs.each do |pair|
+                add_offense(pair.key) if pair.key.sym_type? && pair.key.value == :vcr
+              end
+            end
+          end
+        end
+
+        def allowed_file?
+          ALLOWED_PATH.match?(processed_source.buffer.name)
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cops/custom/disallow_vcr_outside_client_specs_spec.rb
@@ -0,0 +1,70 @@
+require_relative "../../../spec_helper"
+require_relative "../../../../rubocop/cops/custom/disallow_vcr_outside_client_specs"
+
+RSpec.describe RuboCop::Cops::Custom::DisallowVcrOutsideClientSpecs do
+  let(:config) { RuboCop::Config.new("Custom/DisallowVcrOutsideClientSpecs" => {"Enabled" => true}) }
+  let(:cop) { described_class.new(config) }
+
+  it "registers an offense for VCR.use_cassette" do
+    expect_offense(<<~RUBY, "spec/models/widget_spec.rb")
+      VCR.use_cassette("widget") { Widget.fetch }
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^ Only use VCR cassettes in spec/clients.
+    RUBY
+  end
+
+  it "registers an offense for VCR.insert_cassette" do
+    expect_offense(<<~RUBY, "spec/models/widget_spec.rb")
+      VCR.insert_cassette("widget")
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Only use VCR cassettes in spec/clients.
+    RUBY
+  end
+
+  it "registers an offense for the :vcr metadata symbol" do
+    expect_offense(<<~RUBY, "spec/lib/s3/multipart_upload_spec.rb")
+      RSpec.describe S3::MultipartUpload, :vcr do
+                                          ^^^^ Only use VCR cassettes in spec/clients.
+      end
+    RUBY
+  end
+
+  it "registers an offense for the vcr: metadata pair" do
+    expect_offense(<<~RUBY, "spec/models/widget_spec.rb")
+      it "fetches", vcr: { record: :none } do
+                    ^^^ Only use VCR cassettes in spec/clients.
+      end
+    RUBY
+  end
+
+  it "does not register an offense for VCR.configure" do
+    expect_no_offenses(<<~RUBY, "spec/models/widget_spec.rb")
+      VCR.configure do |config|
+        config.hook_into :webmock
+      end
+    RUBY
+  end
+
+  it "does not register an offense in spec/clients" do
+    expect_no_offenses(<<~RUBY, "spec/clients/widget_client_spec.rb")
+      RSpec.describe WidgetClient, :vcr do
+        it "fetches" do
+          VCR.use_cassette("widget") { WidgetClient.fetch }
+        end
+      end
+    RUBY
+  end
+
+  it "does not register an offense for unrelated constants or symbols" do
+    expect_no_offenses(<<~RUBY, "spec/models/widget_spec.rb")
+      Widget.find(:vcr_id)
+      VCRecorder.use_cassette("widget")
+    RUBY
+  end
+
+  it "does not register an offense for :vcr outside RSpec metadata" do
+    expect_no_offenses(<<~RUBY, "spec/models/widget_spec.rb")
+      do_something(:vcr)
+      value = :vcr
+      config = { vcr: true }
+    RUBY
+  end
+end
